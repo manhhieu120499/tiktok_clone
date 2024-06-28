72dc29b7b99f8964618c3b9d0a20723d16b1b1d0
manhhieu1204 <vomanhhieu62@gmail.com>
2024-06-28 12:39:01 +0700

    add new video music

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,13 +1,13 @@
 import videoKay from '~/assets/videos/kay.mp4';
 import videoIT from '~/assets/videos/ongchuIT.mp4';
-// import videoNonstop from '~/assets/videos/nonstop.mp4';
+import videoNonstop from '~/assets/videos/nonstop.mp4';
 import videoKieuChi from '~/assets/videos/kieuchi.mp4';
 import videoHong from '~/assets/videos/hong.mp4';
 import videoVistu from '~/assets/videos/vistu.mp4';
 const videos = {
     kay: videoKay,
     it: videoIT,
-    // nonstop: videoNonstop,
+    nonstop: videoNonstop,
     kieuChi: videoKieuChi,
     hong: videoHong,
     vistu: videoVistu,
